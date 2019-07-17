2faee9142110f6c125caa85726bce0eda230b77f
sean-attewell <sean.attewell@gmail.com>
2019-07-17 15:06:40 +0100

    MVP

--- src/conways.py
+++ src/conways.py
@@ -4,13 +4,92 @@ import pygame, random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (25, 25, 25)
-WIN_SIZE = 500
+RED = (255, 0, 0)
+MARGIN = 3
+SQ_LENGTH = 20
+SQ_NUM = 25
+WIN_SIZE = (SQ_NUM + 1) * MARGIN + SQ_NUM * SQ_LENGTH
 
 pygame.init()
  
 # Set the width and height of the screen [width, height]
 size = (WIN_SIZE, WIN_SIZE)
 screen = pygame.display.set_mode(size)
+
+automata = [0] * (SQ_NUM * SQ_NUM)
+
+# Assign Random Values to our Automata
+
+# for i in range(SQ_NUM * SQ_NUM):
+#     automata[i] = random.randint(0, 1)
+
+for row in range(SQ_NUM):
+    for col in range(SQ_NUM):
+        automata[row * SQ_NUM + col] = random.randint(0, 1)
+
+
+# Update State ( Add Rules to update each cell based on it's previous state )
+
+# Create a new automata for the next state
+new_automata = [0] * (SQ_NUM * SQ_NUM)
+
+for i in range(len(automata)):
+    live = 0
+    dead = 8
+
+    # look at neighbors
+    # (8 if conditions)
+
+    # left Neighbour
+    if i - 1 >= 0 and automata[i - 1]:
+        live += 1
+    # right Neighbour 
+    if i + 1 < (SQ_NUM * SQ_NUM) and automata[i + 1]:
+        live += 1
+    # Below Neighbour
+    if (i + SQ_NUM) < (SQ_NUM * SQ_NUM) and automata[i + SQ_NUM]:
+        live += 1
+    # Above Neighbour
+    if (i - SQ_NUM) >= 0 and automata[i - SQ_NUM]:
+        live += 1
+    # Top Left Neighbour
+    if (i - SQ_NUM - 1) >= 0 and automata[i - SQ_NUM - 1]:
+        live += 1
+    # Top Right Neighbour
+    if (i - SQ_NUM + 1) >= 0 and automata[i - SQ_NUM + 1]:
+        live += 1
+    # Bottom Left Neighbour
+    if (i + SQ_NUM - 1) < (SQ_NUM * SQ_NUM) and automata[i + SQ_NUM - 1]:
+        live += 1
+    # Bottom Right Neighbour
+    if (i + SQ_NUM + 1) < (SQ_NUM * SQ_NUM) and automata[i + SQ_NUM + 1]:
+        live += 1    
+
+    # Update State
+    # if there are less than 2 living neighbors the cell dies
+    if automata[i] and live < 2:
+        new_automata[i] = 0
+    # if alive and has less than 4 neighbors then cell carrys on living
+    elif automata[i] and live < 4:
+        new_automata[i] = 1
+    # TODO: 3 more conditions
+    # if alive and has 4 or more neighbours, the cells dies
+    elif automata[i] and live < 9:
+        new_automata[i] = 0
+    # if dead and exactly 3 neighbours, come to life
+    elif not automata[i] and live == 3:
+        new_automata[i] = 1
+    # else cell is dead and has no reason to come to life:    
+    else:
+        new_automata[i] = 0
+
+# swap the data for the next generations data
+automata = new_automata
+
+
+
+
+
 
 # Add a title
 pygame.display.set_caption("Conway's Game of Life")
@@ -37,10 +116,20 @@ while not done:
     # Here, we clear the screen to gray. Don't put other drawing commands
     # above this, or they will be erased with this command.
     screen.fill(GRAY)
- 
     # --- Drawing code should go here
-   
-
+    # pygame.draw.rect(screen, RED, pygame.Rect(20, 20, 20, 20))
+    y = MARGIN
+    i = 0
+    while y < WIN_SIZE:
+        x = MARGIN
+        while x < WIN_SIZE:
+            if automata[i] == 0:
+                pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, SQ_LENGTH, SQ_LENGTH))
+            else:
+                pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, SQ_LENGTH, SQ_LENGTH))
+            i += 1
+            x += SQ_LENGTH + MARGIN
+        y += SQ_LENGTH + MARGIN
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
